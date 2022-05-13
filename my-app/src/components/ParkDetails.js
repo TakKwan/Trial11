3aaaa48d6d9507df603e7eb1a7d7b42d2118df3d
TakKwan <waslf3651@gmail.com>
2022-05-13 13:23:11 -0400

    stop when scrolled on images

--- my-app/src/components/ParkDetails.js
+++ my-app/src/components/ParkDetails.js
@@ -7,6 +7,7 @@ const ParkDetails = () => {
   const [park, setPark] = useState()
   const { parkCode } = useParams()
   const imageContainer = useRef()
+  const [intervalId, setIntervalId] = useState()
 
   const fetchPark = async () => {
     const respond = await getPark(parkCode)
@@ -17,17 +18,28 @@ const ParkDetails = () => {
     if (imageContainer.current.scrollLeft + imageContainer.current.offsetWidth >= imageContainer.current.scrollWidth) {
       imageContainer.current.scrollLeft = 0
     }
-    imageContainer.current.scrollLeft += 1.5;
+    imageContainer.current.scrollLeft += 1;
   };
+
+  const manualScrollHandler = () => {
+    clearInterval(intervalId)
+  }
+
+  const autoScroll = () => {
+    const newIntervalId = setInterval(() => window.requestAnimationFrame(scroll), 1000 / 60)
+    setIntervalId(newIntervalId);
+    return newIntervalId
+  }
 
   useEffect(() => {
     fetchPark()
-    setInterval(() => window.requestAnimationFrame(scroll), 1000 / 60)
+    const intervalId = autoScroll()
+    return () => clearInterval(intervalId)
   }, [])
 
   return (
     <div className="parkDetails">
-      <div className="imageContainer" ref={imageContainer}>
+      <div className="imageContainer" ref={imageContainer} onWheel={manualScrollHandler}>
         {park?.images.map((image, index) =>
           <img src={image.url} alt="" className="parkImage" key={index} />
         )}
