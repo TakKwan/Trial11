5bae5e641e392cfb099250bfb0874fe72ac3d811
TakKwan <waslf3651@gmail.com>
2022-05-13 16:38:46 -0400

    added watch and favorite icons

--- my-app/src/components/ParkDetails.js
+++ my-app/src/components/ParkDetails.js
@@ -2,16 +2,21 @@ import { useState, useEffect, useRef } from "react"
 import { getPark } from "../services"
 import { useParams } from "react-router-dom"
 import { addToWatchList, addToFavorites, unwatch, removeFavorite } from "../services"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar, faEye } from "@fortawesome/free-solid-svg-icons"
+import { faStar as offStar, faEye as offEye } from "@fortawesome/free-regular-svg-icons"
 
 const ParkDetails = ({ userId }) => {
-  const [park, setPark] = useState()
-  const { parkCode } = useParams()
-  const imageContainer = useRef()
-  const [intervalId, setIntervalId] = useState()
-  const [favorited, setFavorited] = useState()
+  const [park, setPark] = useState(null)
+  const { parkCode } = useParams(null)
+  const imageContainer = useRef(null)
+  const [intervalId, setIntervalId] = useState(null)
+  const [favorited, setFavorited] = useState(false)
+  const [watched, setWatched] = useState(false)
 
   useEffect(() => {
     fetchPark()
+
     const intervalId = autoScroll()
     return () => clearInterval(intervalId)
   }, [])
@@ -49,10 +54,10 @@ const ParkDetails = ({ userId }) => {
       </div>
       <div className="contentContainer">
         <h2 className="details title">{park?.fullName}</h2>
-        {userId && <button onClick={() => addToFavorites(userId, park.parkCode)}>Favorite</button>}
-        {userId && <button onClick={() => removeFavorite(userId, park.parkCode)}>Remove Favorite</button>}
-        {userId && <button onClick={() => addToWatchList(userId, park.parkCode)}>Watch</button>}
-        {userId && <button onClick={() => unwatch(userId, parkCode)}>unwatch</button>}
+        {favorited ? <FontAwesomeIcon icon={faStar} className="fa-2xl fa-star" onClick={() => { setFavorited(false); removeFavorite(userId, park.parkCode) }} /> :
+          <FontAwesomeIcon icon={offStar} className="fa-2xl fa-star" onClick={() => { setFavorited(true); addToFavorites(userId, park.parkCode) }} />}
+        {watched ? <FontAwesomeIcon icon={faEye} className="fa-2xl fa-eye" onClick={() => { setWatched(false); unwatch(userId, park.parkCode) }} /> :
+          <FontAwesomeIcon icon={offEye} className="fa-2xl fa-eye" onClick={() => { setWatched(true); addToWatchList(userId, park.parkCode) }} />}
         <h3>Description</h3>
         <p className="detailsDescription">{park?.description}</p>
         <h3>Weather</h3>
