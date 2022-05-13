fffbd63a0ab778eea6c68fbbdd99c2d50a110197
TakKwan <waslf3651@gmail.com>
2022-05-13 12:01:15 -0400

    images auto scroll

--- my-app/src/components/ParkDetails.js
+++ my-app/src/components/ParkDetails.js
@@ -6,22 +6,25 @@ import { addToWatchList } from "../services"
 const ParkDetails = () => {
   const [park, setPark] = useState()
   const { parkCode } = useParams()
-  const ref = useRef()
+  const imageContainer = useRef()
 
   const fetchPark = async () => {
     const respond = await getPark(parkCode)
     setPark(respond)
   }
 
-
+  const scroll = () => {
+    imageContainer.current.scrollLeft += 1.5;
+  };
 
   useEffect(() => {
     fetchPark()
+    setInterval(() => window.requestAnimationFrame(scroll), 1000 / 60)
   }, [])
 
   return (
     <div className="parkDetails">
-      <div className="imagesContainer">
+      <div className="imageContainer" ref={imageContainer}>
         {park?.images.map((image, index) =>
           <img src={image.url} alt="" className="parkImage" key={index} />
         )}
@@ -29,6 +32,7 @@ const ParkDetails = () => {
       <div className="contentContainer">
         <h2 className="details title">{park?.fullName}</h2>
         <p className="detailsDescription">{park?.description}</p>
+        <button onClick={() => scroll(1)}>click me</button>
       </div>
     </div>
   )
