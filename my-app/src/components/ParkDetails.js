879c3084239bcce8b5261c0c45b9a2c51ef5b149
TakKwan <waslf3651@gmail.com>
2022-05-13 20:55:49 -0400

    auto scroll throttle

--- my-app/src/components/ParkDetails.js
+++ my-app/src/components/ParkDetails.js
@@ -5,14 +5,19 @@ import { addToWatchList, addToFavorites, unwatch, removeFavorite } from "../serv
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar, faEye } from "@fortawesome/free-solid-svg-icons"
 import { faStar as offStar, faEye as offEye } from "@fortawesome/free-regular-svg-icons"
+import { throttleFunction, debounce } from "../utils"
+
+const intervals = []
 
 const ParkDetails = ({ userId }) => {
-  const [park, setPark] = useState(null)
+  const imageWrapper = useRef(null)
   const { parkCode } = useParams(null)
-  const imageContainer = useRef(null)
+  const [park, setPark] = useState(null)
   const [intervalId, setIntervalId] = useState(null)
   const [favorited, setFavorited] = useState(false)
+  const [favIcon, setFavIcon] = useState(offStar)
   const [watched, setWatched] = useState(false)
+  const [watchIcon, setWatchIcon] = useState(offEye)
 
   useEffect(() => {
     fetchPark()
@@ -27,37 +32,80 @@ const ParkDetails = ({ userId }) => {
   }
 
   const scroll = () => {
-
-    if (imageContainer.current?.scrollLeft + imageContainer.current?.offsetWidth >= imageContainer.current?.scrollWidth) {
-      imageContainer.current.scrollLeft = 0
+    if (imageWrapper.current?.scrollLeft + imageWrapper.current?.offsetWidth >= imageWrapper.current?.scrollWidth) {
+      imageWrapper.current.scrollLeft = 0
     }
-    imageContainer.current.scrollLeft += 1;
-  };
-
-  const manualScrollHandler = () => {
-    clearInterval(intervalId)
+    imageWrapper.current.scrollLeft += 1;
   }
+
+  const manualScrollHandler = throttleFunction(() => {
+    clearInterval(intervalId)
+    setTimeout(autoScroll, 10000)
+  }, 100000)
 
   const autoScroll = () => {
     const newIntervalId = setInterval(() => window.requestAnimationFrame(scroll), 1000 / 60)
     setIntervalId(newIntervalId);
+    intervals.push(newIntervalId);
     return newIntervalId
   }
 
+  const toggleFavorite = () => {
+    if (favorited) {
+      setFavorited(false)
+      removeFavorite(userId, park.parkCode)
+      setFavIcon(offStar)
+    } else {
+      setFavorited(true)
+      addToFavorites(userId, park.parkCode)
+      setFavIcon(faStar)
+    }
+  }
+
+  const toggleWatch = () => {
+    if (watched) {
+      setWatched(false)
+      unwatch(userId, park.parkCode)
+      setWatchIcon(offEye)
+    } else {
+      setWatched(true)
+      addToWatchList(userId, park.parkCode)
+      setWatchIcon(faEye)
+    }
+  }
+
+  const hoverFavIcon = () => {
+    setFavIcon(favIcon === faStar ? offStar : faStar)
+  }
+
+  const exitFavIcon = () => {
+    setFavIcon(favorited ? faStar : offStar)
+  }
+
+  const hoverWatchIcon = () => {
+    setWatchIcon(watchIcon === faEye ? offEye : faEye)
+  }
+
+  const exitWatchIcon = () => {
+    setWatchIcon(watched ? faEye : offEye)
+  }
 
   return (
     <div className="parkDetails">
-      <div className="imageContainer" ref={imageContainer} onWheel={manualScrollHandler}>
-        {park?.images.map((image, index) =>
-          <img src={image.url} alt="" className="parkImage" key={index} />
-        )}
+      <div className="imageContainer" >
+        <div className="groupButtons">
+          <FontAwesomeIcon icon={favIcon} className="fa-2xl fa" onClick={toggleFavorite} onMouseEnter={hoverFavIcon} onMouseLeave={exitFavIcon} />
+          <FontAwesomeIcon icon={watchIcon} className="fa-2xl fa" onClick={toggleWatch} onMouseEnter={hoverWatchIcon} onMouseLeave={exitWatchIcon} />
+        </div>
+        <div className="imageWrapper" ref={imageWrapper} onWheel={manualScrollHandler}>
+          {park?.images.map((image, index) =>
+            <img src={image.url} alt="" className="parkImage" key={index} />
+          )}
+        </div>
       </div>
+
       <div className="contentContainer">
         <h2 className="details title">{park?.fullName}</h2>
-        {favorited ? <FontAwesomeIcon icon={faStar} className="fa-2xl fa-star" onClick={() => { setFavorited(false); removeFavorite(userId, park.parkCode) }} /> :
-          <FontAwesomeIcon icon={offStar} className="fa-2xl fa-star" onClick={() => { setFavorited(true); addToFavorites(userId, park.parkCode) }} />}
-        {watched ? <FontAwesomeIcon icon={faEye} className="fa-2xl fa-eye" onClick={() => { setWatched(false); unwatch(userId, park.parkCode) }} /> :
-          <FontAwesomeIcon icon={offEye} className="fa-2xl fa-eye" onClick={() => { setWatched(true); addToWatchList(userId, park.parkCode) }} />}
         <h3>Description</h3>
         <p className="detailsDescription">{park?.description}</p>
         <h3>Weather</h3>
